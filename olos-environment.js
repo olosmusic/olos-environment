8c1dde5cb36780b16c7a267a76aa0c57049aa631
Jason Sigal <jason.sigal@gmail.com>
2015-02-22 01:17:52 -0500

    events

--- olos-environment.js
+++ olos-environment.js
@@ -1,15 +1,14 @@
-(function(params){
-
-  // init draggability
-  // TO DO: add this to the base element so that everything is draggable
-
-  var oscType = 'sine';
-  var freq = 440;
+// (function(params){
 
   Polymer('olos-environment', {
+
     ready: function() {
       console.log('ready!');
+      this.addEventListener('click', this.clickCallback);
+    },
+    clickCallback: function(e) {
+      console.log(e);
     }
   });
 
-})();
+// })();
