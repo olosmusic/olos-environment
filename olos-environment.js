d6faa3aa4007a1e38684ed7103b85b8a393b2f3b
Jason Sigal <jason.sigal@gmail.com>
2015-02-21 23:43:42 -0500

    first commit

---
+++ olos-environment.js
@@ -0,0 +1,15 @@
+(function(params){
+
+  // init draggability
+  // TO DO: add this to the base element so that everything is draggable
+
+  var oscType = 'sine';
+  var freq = 440;
+
+  Polymer('olos-environment', {
+    ready: function() {
+      console.log('ready!');
+    }
+  });
+
+})();
