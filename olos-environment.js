cbf8756893b66eba59df49a2bb82383766dea3bc
therewasaguy <jason.sigal@gmail.com>
2015-02-22 21:28:27 -0500

    add interact.js for dragndrop

--- olos-environment.js
+++ olos-environment.js
@@ -34,8 +34,8 @@
       var self = this,
       newEl = document.createElement(newElName);
       // TO DO: check to make sure element is loaded, otherwise load it
-      newEl.setAttribute('_top', top);
-      newEl.setAttribute('_left', left);
+      // newEl.setAttribute('_top', top);
+      // newEl.setAttribute('_left', left);
       newEl.innerHTML = 'hello';
       self._processChild(newEl);
       // self.$.container.appendChild(newEl);
@@ -61,26 +61,72 @@
       // var children = self.$.container.querySelectorAll('*');
       // [].forEach.call(children, function(el, i) {
       var el = newChild;
-      console.log(el);
-      var droppedTop = el.getAttribute('_top');
-      var droppedLeft = el.getAttribute('_left');
 
-      // name
-      var elName = el.tagName.toLowerCase();
 
-      // Set unique ID to each element
-      el.id = el.id; //|| self.generateUniqueId(elName);
+      // interact.js
+      interact(el).draggable({
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+          // restriction: "parent",
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+        onstart: self.dragStart,
+        onmove: self.dragMove,
+        onend: self.dragEnd
+      });
 
-      var elItem = {  element: el,
-                      id: el.id,
-                      name: elName,
-                      top: droppedTop,
-                      left: droppedLeft
-      };
-      self._elements.push(elItem);
-      console.log(self._elements);
+      // // DELETE
+      // console.log(el);
+      // var droppedTop = el.getAttribute('_top');
+      // var droppedLeft = el.getAttribute('_left');
+
+      // // name
+      // var elName = el.tagName.toLowerCase();
+
+      // // Set unique ID to each element
+      // el.id = el.id; //|| self.generateUniqueId(elName);
+
+      // var elItem = {  element: el,
+      //                 id: el.id,
+      //                 name: elName,
+      //                 top: droppedTop,
+      //                 left: droppedLeft
+      // };
+      // self._elements.push(elItem);
+      // console.log(self._elements);
       self.$.container.appendChild(el);
-    }
+    },
+
+    // interact.js
+    dragStart: function(event) {
+      console.log(event);
+      console.log('drag start!');
+    },
+
+    dragMove: function(event) {
+      var target = event.target.$.container;
+      console.log(target);
+      // keep the dragged position in the data-x/data-y attributes
+      var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+      var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+      // translate the element
+      target.style.webkitTransform =
+      target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
+      // update the posiion attributes
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+
+      // console.log('onmove!');
+    },
+
+    dragEnd: function(event) {
+      console.log(event);
+      console.log('onend!');
+    },
 
   });
 
